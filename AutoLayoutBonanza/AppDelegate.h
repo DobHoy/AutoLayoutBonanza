a1981a3ba50a0bbcda817b888084cca12c6c0f20
Matt Hanlon <mhanlon@gmail.com>
2015-01-07 21:51:58 -0800

    Adding our project files.

---
+++ AutoLayoutBonanza/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutBonanza
+//
+//  Created by Matthew Hanlon on 1/7/15.
+//  Copyright (c) 2015 Q.I. Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutBonanza/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutBonanza
+//
+//  Created by Matthew Hanlon on 1/7/15.
+//  Copyright (c) 2015 Q.I. Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
